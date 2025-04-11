c2cbc4eb28a88f8e29739ab70f9f0640cdc391d1
IOMisaka <zekashima@qq.com>
2025-04-11 22:03:47 +0800

    init

---
+++ ddns.py
@@ -0,0 +1,65 @@
+import httpx,socket
+import time,json
+#JSON配置文件
+"""
+{
+     "zone_id":"xxxxxx",
+     "token":"xxxxxxx",
+     "domain":"example.com",
+     "record_id":"xxxxxxxx"
+}
+"""
+config = json.loads(open('config.json','r',encoding='utf-8').read())
+print(config)
+def get_ipv6():
+    #获取本机ip
+    addrs = socket.getaddrinfo(socket.gethostname(),None,socket.AF_INET6)
+    ipv6_list = [addr[4][0] for addr in addrs]
+    # 按优先级排序
+    sorted_ips = sorted(ipv6_list, key=lambda ip: (
+        0 if ip.startswith('2') or ip.startswith('3') else  # 优先 GUA
+        1 if ip.startswith('fd') else  # 最后 ULA
+        2  # 其他（如链路本地地址 fe80::/10）
+    ))
+    return sorted_ips[0] if sorted_ips else None
+def cf_get_record_id():
+    url = f'https://api.cloudflare.com/client/v4/zones/{config["zone_id"]}/dns_records'
+    headers = {
+        'Authorization': f'Bearer {config["token"]}',
+        'Content-Type': 'application/json'
+    }
+    response = httpx.get(url, headers=headers)
+    return response.json()['result'].find(lambda x:x['name']==config["domain"])['id']
+def cf_ddns(ipv6):          
+    url = f'https://api.cloudflare.com/client/v4/zones/{config["zone_id"]}/dns_records/{config["record_id"]}'
+    headers = {
+        'Authorization': f'Bearer {config["token"]}',
+        'Content-Type': 'application/json'
+    }
+    data = {
+        'type': 'AAAA',
+        'name': config["domain"],
+        'content': ipv6,
+        'ttl': 1
+    }
+    response = httpx.patch(url, headers=headers, json=data)
+
+def main():
+    current_ip = None
+    while True:
+        try:
+            ipv6 = get_ipv6()
+            if ipv6 != current_ip:
+                print(f"IP changed:{current_ip} -> {ipv6}")
+                try:
+                    cf_ddns(ipv6)
+                    current_ip = ipv6
+                    print("Updated CloudFlare DNS record successfully.")
+                except Exception as e:
+                    print("Failed to update CloudFlare DNS record:", e)
+        except Exception as e:
+            print(e)
+        time.sleep(60)
+
+if __name__ == "__main__":
+    main()
